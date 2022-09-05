2c447e4f0385b60cca5a515074d5edc85a308300
Wirlie <josue.211996@outlook.com>
2022-09-04 23:46:50 -0500

    Remove BungeeCord code from v1
    No longer required.

--- Platform-BungeeCord/src/main/kotlin/dev/wirlie/bungeecord/glist/util/PlayerGlistEntry.kt
+++
@@ -1,6 +0,0 @@
-package dev.wirlie.bungeecord.glist.util
-
-import dev.wirlie.bungeecord.glist.activity.ActivityType
-import net.md_5.bungee.api.connection.ProxiedPlayer
-
-class PlayerGlistEntry(val player: ProxiedPlayer, val prefix: String, var activities: MutableSet<ActivityType>)
