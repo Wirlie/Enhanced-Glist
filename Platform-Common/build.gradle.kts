c8674418c44d40c2c47325ff7c0c1c36a29b7420
Wirlie <josue.211996@outlook.com>
2022-09-12 02:18:24 -0500

    WIP for Protocolize support (not functional at this moment)

--- Platform-Common/build.gradle.kts
+++ Platform-Common/build.gradle.kts
@@ -9,16 +9,21 @@ repositories {
     maven {
         url = uri("https://oss.sonatype.org/content/repositories/snapshots")
     }
+    maven {
+        url = uri("https://mvn.exceptionflug.de/repository/exceptionflug-public/")
+    }
 }
 
 dependencies {
-    compileOnly("net.luckperms:api:5.4")
-    compileOnly("com.github.LeonMangler:PremiumVanish:6.2.6-4")
     implementation("net.kyori:adventure-api:4.11.0")
     implementation("net.kyori:adventure-text-minimessage:4.11.0")
     implementation("net.kyori:adventure-text-serializer-legacy:4.11.0")
-    compileOnly("net.luckperms:api:5.4")
     implementation("com.google.code.gson:gson:2.9.1")
+
+    compileOnly("net.luckperms:api:5.4")
+    compileOnly("com.github.LeonMangler:PremiumVanish:6.2.6-4")
+    compileOnly("net.luckperms:api:5.4")
+    compileOnly("dev.simplix:protocolize-api:2.2.2")
 
     // Caffeine for Cache
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.1")
