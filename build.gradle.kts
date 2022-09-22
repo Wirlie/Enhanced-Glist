eecd16afd1e575627345253913048a191447a807
Wirlie <josue.211996@outlook.com>
2022-09-22 01:38:26 -0500

    Set kotlin to Java 11 [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,12 +53,24 @@ subprojects {
         targetCompatibility = "11"
     }
 
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     sourceCompatibility = "11"
     targetCompatibility = "11"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 // Only add Publishing task to -API projects
