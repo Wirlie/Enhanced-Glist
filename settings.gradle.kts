e3d50bd404519ad38f6f3afd3ea5a94f05d270c9
Wirlie <wirlie.dev@gmail.com>
2024-01-04 00:58:05 -0600

    Use lowercase for artifact ids

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,23 @@
 include("Platform-BungeeCord")
-project(":Platform-BungeeCord").name = "EnhancedGlist-BungeeCord"
+project(":Platform-BungeeCord").name = "enhancedglist-bungeecord"
 
 include("Platform-BungeeCord-API")
-project(":Platform-BungeeCord-API").name = "EnhancedGlist-BungeeCord-API"
+project(":Platform-BungeeCord-API").name = "enhancedglist-bungeecord-api"
 
 include("Platform-Spigot")
-project(":Platform-Spigot").name = "EnhancedGlist-Spigot-Bridge"
+project(":Platform-Spigot").name = "enhancedglist-spigot-bridge"
 
 include("Platform-Common")
-project(":Platform-Common").name = "EnhancedGlist-Common"
+project(":Platform-Common").name = "enhancedglist-common"
 
 include("Platform-Velocity")
-project(":Platform-Velocity").name = "EnhancedGlist-Velocity"
+project(":Platform-Velocity").name = "enhancedglist-velocity"
 
 include("Platform-Velocity-API")
-project(":Platform-Velocity-API").name = "EnhancedGlist-Velocity-API"
+project(":Platform-Velocity-API").name = "enhancedglist-velocity-api"
 
 include("Updater")
-project(":Updater").name = "EnhancedGlist-Updater"
+project(":Updater").name = "enhancedglist-updater"
 
 include("Messenger")
-project(":Messenger").name = "EnhancedGlist-Messenger"
+project(":Messenger").name = "enhancedglist-messenger"
